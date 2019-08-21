27dcd7e257836b67e74be3c0488856dbdafee5de
ChenYong <25790987+chenyong@users.noreply.github.com>
2019-08-21 04:41:19 +0000

    Initial commit

---
+++ example/main.tsx
@@ -0,0 +1,31 @@
+import ReactDOM, { unstable_renderSubtreeIntoContainer } from "react-dom";
+import React from "react";
+
+import { parseRoutePath, IRouteParseResult } from "@jimengio/ruled-router";
+
+import { routerRules } from "./models/router-rules";
+
+import Container from "./pages/container";
+import { demo } from "../src/index";
+
+const renderApp = () => {
+  let routerTree = parseRoutePath(window.location.hash.slice(1), routerRules);
+
+  ReactDOM.render(<Container router={routerTree} />, document.querySelector(".app"));
+};
+
+window.onload = renderApp;
+
+window.addEventListener("hashchange", () => {
+  renderApp();
+});
+
+demo();
+
+declare var module: any;
+
+if (module.hot) {
+  module.hot.accept([], () => {
+    renderApp();
+  });
+}
